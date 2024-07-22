a3994e688f88bb3fbc34d84818bf0122f9421aa2
Foxster <huythefox@gmail.com>
2024-07-21 19:51:25 -0700

    Remove roothide

--- src/Tweak.h
+++ src/Tweak.h
@@ -1,6 +1,6 @@
 // #import <sys/utsname.h>
 #import <rootless.h>
-#include <roothide.h>
+// #include <roothide.h>
 #import <YTHeaders/YTPlayerViewController.h>
 #import <YTHeaders/YTPlayerView.h>
 #import <YTHeaders/YTMainAppVideoPlayerOverlayViewController.h>
